496e069e7205fe44cb3f73959bae1773e6ed1044
Kostiantyn Nikolayev <nikolayev.kostiantyn@greenroad.com>
2023-04-09 22:23:58 +0300

    Redis drop keys scripts

---
+++ redis/drop_all_keys.lua
@@ -0,0 +1,1 @@
+192.168.1.191:30007> EVAL "local keys = redis.call('keys', ARGV[1]) \n for i=1,#keys,5000 do \n redis.call('del', unpack(keys, i, math.min(i+4999, #keys))) \n end \n return keys" 0  {dynamic:Grt* 
---
+++ redis/dropkeys.cmd
@@ -0,0 +1,2 @@
+@echo off
+for /f "delims=" %%i in ('redis-cli.exe -h %1 -p %2 keys %3') do redis-cli.exe -h %1 -p %2 del "%%i" >nul
